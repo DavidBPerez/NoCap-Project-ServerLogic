b554286340224e17dbb0ca4fc9ba12b84fc24ebd
DavidBPerez <77704364+DavidBPerez@users.noreply.github.com>
2023-10-14 07:39:37 +0000

    added more app logic & client events

--- game_src/main.cpp
+++ game_src/main.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main() {
-  cout << "Hello :) Pretend this is the game." << endl;
+  string cmd;
+  cout << "Hello :) Pretend this is the game menu." << endl;
 
+  while (true) {
+    cin >> cmd;
+
+    if(cmd == "start") {
+      cout << "Starting game..." << endl;
+    } else if (cmd == "exit") {
+      cout << "Exiting game..." << endl;
+      break;
+    }
+  }
+  
   return 0;
 }
--- index.js
+++ index.js
@@ -8,23 +8,35 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Define the path to your game executable
+// creates 'game' process
 const gamePath = path.resolve(__dirname, './game_src/main');
 console.log('Game path:', gamePath);
 
-// Start the game process when the server starts
 const gameProcess = spawn(gamePath, []);
 
+// tracks messages (data)
 gameProcess.stdout.on('data', (data) => {
   const message = data.toString();
   console.log('Received from game:', message);
   io.emit('message-from-game', message);
 });
 
+// tracks client (event listeners)
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('Client connected');
+
+  socket.on('start-application', () => {
+    console.log('Starting application.');
+    gameProcess.stdin.write('start\n');
+  });
+
+  socket.on('exit-application', () => {
+    console.log('Exiting application.');
+    gameProcess.stdin.write('exit\n')
+  });
+  
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    console.log('Client disconnected');
   });
 });
 
